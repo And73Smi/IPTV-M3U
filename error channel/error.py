1bfd4b75c0ca959a7d0c52ce19cefb1214c37ce8
And73Smi <161311212+And73Smi@users.noreply.github.com>
2024-06-17 18:46:16 +0300

    Update error.py

--- error channel/error.py
+++ error channel/error.py
@@ -1,15 +1,15 @@
 import requests
-from iptv.m3u import iptv.m3u
+import m3u8
 
 # Αντικαταστήστε με τη διαδρομή του αρχείου m3u
-m3u_file_path = 'iptv.m3u'
+m3u_file_path = 'path/to/your/iptv.m3u'
 
 # Φόρτωση και ανάλυση του αρχείου m3u
-with open(iptv.m3u, 'r') as file:
-    m3u_content = M3U8(file.read())
+with open(m3u_file_path, 'r') as file:
+    m3u_content = m3u8.loads(file.read())
 
 # Έλεγχος κάθε καναλιού
 for channel in m3u_content.segments:
     response = requests.head(channel.uri)
     if response.status_code != 200:
-        print(f'Error: Channel {no work} is not working')
+        print(f'Error: Channel {channel.uri} is not working')
